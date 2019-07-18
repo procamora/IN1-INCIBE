4c965bf5d52ec63993448bfe422ed949f639d6ff
José María <josemaria.jorquera@um.es>
2019-07-18 12:53:25 +0200

    Cambio de niveles ThreatLevel

--- Parser/threatLevel.py
+++ Parser/threatLevel.py
@@ -9,13 +9,13 @@ class ThreatLevel(object):
     def __init__(self) -> NoReturn:
         super().__init__()
 
-        self._LEVEL_3 = ['cd', 'cat', 'chkconfig', 'echo', 'du', 'df', 'uptime', 'w', 'whoami', 'ifconfig',
+        self._LEVEL_1 = ['cd', 'cat', 'chkconfig', 'echo', 'du', 'df', 'uptime', 'w', 'whoami', 'ifconfig',
                          'netstat', 'last', 'ls', 'ulimit', 'uname', 'history', 'unset', 'set', 'export']
 
         self._LEVEL_2 = ['sed', 'tar', 'if', 'while', 'do', 'chmod', 'cp', 'mkdir', 'mv', 'apt-get', 'apt',
                          'touch', 'yum', 'dnf', 'passwd', 'mount', 'chown', 'bzip2', 'make', 'gcc', 'dd']
 
-        self._LEVEL_1 = ['wget', 'ssh', 'tftp', 'tftpd', 'scp', 'python', 'perl', 'nc', 'curl', 'ftpget',
+        self._LEVEL_3 = ['wget', 'ssh', 'tftp', 'tftpd', 'scp', 'python', 'perl', 'nc', 'curl', 'ftpget',
                          'rm', 'sh', 'bash', 'busybox', 'reSuSEfirewall', 'SuSEfirewall', 'killall',
                          'kill', 'pkill', 'sleep', 'sudo', 'nohup', 'poweroff', 'reboot', 'halt', 'exec']
 
@@ -32,14 +32,14 @@ class ThreatLevel(object):
         for commands in list_inputs:
             command = commands.get_input().split(' ')[0]  # Obtenemos solo el comando
             if command in self._LEVEL_1:
-                return 1
+                return 3
 
         for commands in list_inputs:
             command = commands.get_input().split(' ')[0]  # Obtenemos solo el comando
             if command in self._LEVEL_2:
                 return 2
 
-        return 3
+        return 1
 
     def get_threat_level_1(self, list_inputs) -> int:
         if len(list_inputs) == 0:
@@ -48,11 +48,11 @@ class ThreatLevel(object):
         for commands in list_inputs:
             command = commands.split(' ')[0]  # Obtenemos solo el comando
             if command in self._LEVEL_1:
-                return 1
+                return 3
 
         for commands in list_inputs:
             command = commands.split(' ')[0]  # Obtenemos solo el comando
             if command in self._LEVEL_2:
                 return 2
 
-        return 3
+        return 1
